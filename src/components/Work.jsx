31c261a7fc92829eaf1b3562aa825efa3ed11913
Bryce Peters <brycepeters95@icloud.com>
2022-04-07 13:46:07 -0400

    2

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -26,7 +26,7 @@ const Work = () => {
                 React JS Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://vgwagers.com/'>
+                <a href='https://videogamewagers.herokuapp.com/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
